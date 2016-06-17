c166ffbde8e599a43a97fbea8f41121e96939270
Russ Davies <russdavies392@gmail.com>
2016-06-17 12:00:00 -0700

    Create more variety in vacation results

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
   $("form#vacation").submit(function(event) {
 
-    event.preventDefault();
+
     var person = $("select#person").val();
     var color = $("select#color").val();
     var food = $("select#food").val();
 
 
-      if (person === "Outgoing" && color === "Red" && food === "Meat") {
-      $("#volcano").toggle();
-    } else if (person === "Mysterious" && color === "Black" && food === "Vegetables") {
-      $("#tibet").toggle();
-    } else if (person === "Boring" && color === "Blue" && food === "Sweets") {
-      $("#idaho").toggle();
-    } else {
-      $("#idaho").show();
-    }
-
+    if (person === "Outgoing" && color === "Red" && food != "Vegetables") {
+    $("#volcano").toggle();
+  } else if (person === "Outgoing" && color != "Black" && food === "Sweets") {
+    $("#nice").toggle();
+  } else if (person === "Mysterious" && color != "Red" && food === "Vegetables") {
+    $("#tibet").toggle();
+  } else if (person === "Mysterious" && color != "Blue" && food != "Vegetables") {
+    $("#vampire").toggle();
+  } else if (person === "Average") {
+    $("#idaho").toggle();
+  } else {
+    $("#space").toggle();
+  }
+    event.preventDefault();
   });
 });
