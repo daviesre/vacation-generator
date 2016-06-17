13f6f1ca7b911a1ca5646ee30fdb629980258702
Russ Davies <russdavies392@gmail.com>
2016-06-17 11:16:49 -0700

    Add JQuery to make specific ids appear when clicking submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
   $("form#vacation").submit(function(event) {
 
-
     event.preventDefault();
+    var person = $("select#person").val();
+    var color = $("select#color").val();
+    var food = $("select#food").val();
+
+
+      if (person === "Outgoing" && color === "Red" && food === "Meat") {
+      $("#volcano").toggle();
+    } else if (person === "Mysterious" && color === "Black" && food === "Vegetables") {
+      $("#tibet").toggle();
+    } else if (person === "Boring" && color === "Blue" && food === "Sweets") {
+      $("#idaho").toggle();
+    } else {
+      $("#idaho").show();
+    }
+
   });
 });
