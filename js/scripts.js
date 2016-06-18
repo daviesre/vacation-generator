f1228a63f3e79d3d6c8a511fcb202e698e42cc76
Russ Davies <russdavies392@gmail.com>
2016-06-17 17:46:58 -0700

    Add minor small changes to final part of project

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,22 @@
 $(document).ready(function() {
   $("form#vacation").submit(function(event) {
-    event.preventDefault();
     var person = $("select#person").val();
     var color = $("select#color").val();
     var food = $("select#food").val();
 
     if (person === "Outgoing" && color === "Red" && food === "Meat") {
       $("#volcano").fadeToggle();
-    } else if (person === "Outgoing" && color != "Black" && food != "Meat") {
+     } else if (person === "Outgoing" && color != "Black" && food != "Meat") {
       $("#nice").fadeToggle();
-    } else if (person === "Mysterious" && color != "Red" && food === "Vegetables") {
+     } else if (person === "Mysterious" && color != "Red" && food === "Vegetables") {
       $("#tibet").fadeToggle();
-    } else if (person === "Mysterious" && color != "Blue" && food != "Vegetables") {
+     } else if (person === "Mysterious" && color != "Blue" && food != "Vegetables") {
       $("#vampire").fadeToggle();
-    } else if (person === "Average") {
+     } else if (person === "Average") {
       $("#idaho").fadeToggle();
-    } else {
+     } else {
       $("#space").fadeToggle();
-    }
-
+     }
+      event.preventDefault();
   });
 });
