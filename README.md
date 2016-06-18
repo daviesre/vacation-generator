1f2fe28b22d0c05e102d4b8461bd2b58ccccc498
Russ Davies <russdavies392@gmail.com>
2016-06-17 17:09:39 -0700

    Add a README.md page

---
+++ README.md
@@ -0,0 +1,37 @@
+# Vacation Suggester
+
+#### Finds the right vacation for you, 06.17.2016
+
+#### By Russ Davies
+
+## Description
+
+This helps the user find a vacation right for them. After selecting different choices, they will be presented with a result that matches their personality.
+
+## Setup/Installation Requirements
+
+* Clone this repository
+
+## Known Bugs
+
+Does not clear previous data after a resubmit.
+
+## Support and contact details
+
+_{Let people know what to do if they run into any issues or have questions, ideas or concerns.  Encourage them to contact you or make a contribution to the code.}_
+
+## Technologies Used
+
+HTML
+
+CSS
+
+Javascript
+
+JQuery
+
+Bootstrap
+
+### License
+
+Copyright (c) 2016 **Russ Davies**
--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,23 @@
 $(document).ready(function() {
   $("form#vacation").submit(function(event) {
-
+    event.preventDefault();
     var person = $("select#person").val();
     var color = $("select#color").val();
     var food = $("select#food").val();
 
     if (person === "Outgoing" && color === "Red" && food === "Meat") {
-    $("#volcano").fadeToggle();
-  } else if (person === "Outgoing" && color != "Black" && food != "Meat") {
-    $("#nice").fadeToggle();
-  } else if (person === "Mysterious" && color != "Red" && food === "Vegetables") {
-    $("#tibet").fadeToggle();
-  } else if (person === "Mysterious" && color != "Blue" && food != "Vegetables") {
-    $("#vampire").fadeToggle();
-  } else if (person === "Average") {
-    $("#idaho").fadeToggle();
-  } else {
-    $("#space").fadeToggle();
-  }
-    event.preventDefault();
+      $("#volcano").fadeToggle();
+    } else if (person === "Outgoing" && color != "Black" && food != "Meat") {
+      $("#nice").fadeToggle();
+    } else if (person === "Mysterious" && color != "Red" && food === "Vegetables") {
+      $("#tibet").fadeToggle();
+    } else if (person === "Mysterious" && color != "Blue" && food != "Vegetables") {
+      $("#vampire").fadeToggle();
+    } else if (person === "Average") {
+      $("#idaho").fadeToggle();
+    } else {
+      $("#space").fadeToggle();
+    }
 
   });
 });
